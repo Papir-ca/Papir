03ea637f89fda314eea29b579f17ff4b72380411
Papir-ca <williamsakino@gmail.com>
2026-02-23 15:57:07 -0500

    Update server.js

--- server.js
+++ server.js
@@ -21,12 +21,14 @@ app.use(helmet({
         "'self'",
         "https://cdn.jsdelivr.net",
         "https://cdnjs.cloudflare.com",
+        "https://unpkg.com",
         "'unsafe-inline'",
         "'unsafe-eval'"
       ],
       styleSrc: [
         "'self'",
         "https://cdnjs.cloudflare.com",
+        "https://fonts.googleapis.com",
         "'unsafe-inline'"
       ],
       imgSrc: [
@@ -49,6 +51,7 @@ app.use(helmet({
       fontSrc: [
         "'self'",
         "https://cdnjs.cloudflare.com",
+        "https://fonts.gstatic.com",
         "data:"
       ],
       objectSrc: ["'none'"],
